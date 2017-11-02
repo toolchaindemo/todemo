c8bcd955dd4a82e7e7a8c23e6bf7cead66b60427
Tool Chain Demo <toolchaindemo@gmail.com>
2017-11-02 22:35:06 +0000

    new change to test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World I love you!");
+            Console.WriteLine("Hello World I love you long time!");
         }
     }
 }
